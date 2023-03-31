2a929dbd49a9513862cfa2214de51f6d30dd77cc
Yujin Kim <gettheradio@gmail.com>
2023-03-31 03:44:16 -0400

    change index.js

--- pages/index.js
+++ pages/index.js
@@ -26,33 +26,33 @@ export default function Home() {
                 <Col md={6}>
                     <p>
                         The Metropolitan Museum of Art of New York City,
-                        colloquially `&quot;`the Met`&quot;`, is the largest art
+                        colloquially &quot;the Met&quot;, is the largest art
                         museum in the Americas. Its permanent collection
                         contains over two million works, divided among 17
                         curatorial departments. The main building at 1000 Fifth
                         Avenue, along the Museum Mile on the eastern edge of
-                        Central Park on Manhattan`&apos;`s Upper East Side, is
-                        by area one of the world`&apos;`s largest art museums. A
-                        much smaller second location, The Cloisters at Fort
-                        Tryon Park in Upper Manhattan, contains an extensive
+                        Central Park on Manhattan&apos;s Upper East Side, is by
+                        area one of the world&apos;s largest art museums. A much
+                        smaller second location, The Cloisters at Fort Tryon
+                        Park in Upper Manhattan, contains an extensive
                         collection of art, architecture, and artifacts from
                         medieval Europe.
                     </p>
                     <p>
                         The Metropolitan Museum of Art was founded in 1870 with
                         its mission to bring art and art education to the
-                        American people. The museum`&apos;`s permanent
-                        collection consists of works of art from classical
-                        antiquity and ancient Egypt, paintings, and sculptures
-                        from nearly all the European masters, and an extensive
-                        collection of American and modern art. The Met maintains
-                        extensive holdings of African, Asian, Oceanian,
-                        Byzantine, and Islamic art. The museum is home to
-                        encyclopedic collections of musical instruments,
-                        costumes, and accessories, as well as antique weapons
-                        and armor from around the world. Several notable
-                        interiors, ranging from 1st-century Rome through modern
-                        American design, are installed in its galleries.
+                        American people. The museum&apos;s permanent collection
+                        consists of works of art from classical antiquity and
+                        ancient Egypt, paintings, and sculptures from nearly all
+                        the European masters, and an extensive collection of
+                        American and modern art. The Met maintains extensive
+                        holdings of African, Asian, Oceanian, Byzantine, and
+                        Islamic art. The museum is home to encyclopedic
+                        collections of musical instruments, costumes, and
+                        accessories, as well as antique weapons and armor from
+                        around the world. Several notable interiors, ranging
+                        from 1st-century Rome through modern American design,
+                        are installed in its galleries.
                     </p>
                 </Col>
                 <Col md={6}>
